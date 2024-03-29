b2772b539d92a9220540eaf01050ee96a6ed3fc6
Prabodh Agarwal <prabodh1194@gmail.com>
2024-03-29 09:00:22 +0530

    define OIDC dependency
    Use this command:
    
    `./gradlew addExtension --extensions='oidc'`
    
    to define the dependency.
    
    OIDC dependency defines the goodness to do OIDC based auth.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
+    implementation("io.quarkus:quarkus-oidc")
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
     implementation("io.quarkus:quarkus-arc")
     testImplementation("io.quarkus:quarkus-junit5")
